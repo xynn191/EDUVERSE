db3f48f97a433d031009efeb81b6fed486677ca4
xynn191 <raeilhamutomo@gmail.com>
2025-08-30 12:55:57 +0700

    Add files via upload

---
+++ LandingPage/tesmain.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="tesmain.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="navbar">
+            <h1>EDUVERSE</h1>
+            <ul>
+                <li><a href="#">Layanan Kami</a></li>
+                <li><a href="#">Ulasan</a></li>
+            </ul>
+        </div>
+        <div class="main">
+            <h1>EDUVERSE</h1>
+            <p>Belajar Matematika jadi mudah, seru, dan menyenangkan!<br>
+               Temukan penjelasan jelas, latihan interaktif, <br> dan tips cepat memahami rumus. 
+            </p>
+            <div class="button-container">
+                <a href="/EDUVERSE/home/login.php"><button>Login</button></a>
+                <a href="/EDUVERSE/home/registerasi.php"><button>Register</button></a>
+            </div>
+            <img src="assets landing/Baixar_um_grupo_de_pessoas_compartilhando_histórias_juntas_gratuitamente__1_-removebg-preview.png" alt="" class="main-img">
+        </div>
+
+        <div class="layanan">
+            <h1>Layanan Kami</h1>
+            <div class="cards">
+                <div class="card">
+                    <h2>BIMBEL ONLINE TERBAIK</h2>
+                    <p>Lorem ipsum dolor sit amet, tortor non gravida. Sed vulputate nibh diam, non maximus eros facilisis sed. Donec et ultrices felis. Integer a auctor erat.</p>
+                </div>
+                <div class="card">
+                    <h2>2000+ LULUSAN MASUK PTN</h2>
+                    <p>Lorem ipsum dolor sit amet, tortor non gravida. Sed vulputate nibh diam, non maximus eros facilisis sed. Donec et ultrices felis. Integer a auctor erat.</p>
+                </div>
+                <div class="card">
+                    <h2>GURUNYA GACOR</h2>
+                    <p>Lorem ipsum dolor sit amet, tortor non gravida. Sed vulputate nibh diam, non maximus eros facilisis sed. Donec et ultrices felis. Integer a auctor erat.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="ulasan">
+            
+            <div class="ulasanContainer">
+                <div class="fotoulasan">
+                    <img src="assets landing/Dafiand_Alfadel_1583741195-removebg-preview.png" alt="">
+                </div>
+                <div>
+                    <h2>Dafiand Alfadel</h2> <div></div>
+                    <p>Lorem ipsum dolor sit amet, tortor non gravida. Sed vulputate nibh diam, non maximus eros facilisis sed. Donec et ultrices felis. Integer a auctor erat.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer">
+            <div class="alamat">
+                <h3>EDUVERSE</h3>
+                <p>Jl. Tanah Baru Jl. Kemiri Jaya No.99, Beji, Kecamatan Beji, Kota Depok, Jawa Barat 16421</p>
+            </div>
+            <div class="contact">
+                <h3>Hubungi Kami</h3>
+                <p><img src="assets landing/whatsapp.png" alt="" class="whatsapp">0895322821941</p>
+                <p><img src="assets landing/gmail.png" alt="" class="gmail">eduverse@gmail.com</p>
+                <p><img src="assets landing/instagram-3.png" alt="" class="instagram">@EDUVERSE</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
